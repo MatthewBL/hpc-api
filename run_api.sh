de73aec08539baa646cbcd85d52ea29e418e022b
Matthew BL <xamtehwt@gmail.com>
2025-12-18 03:56:22 +0100

    Corrected run_api.sh file

--- run_api.sh
+++ run_api.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1 -t 168:00:00 --nodelist=c03
 
-# Ensure we run from the repository directory (where this script lives)
-cd "$(dirname "$0")"
-
 npm install
 npm start
